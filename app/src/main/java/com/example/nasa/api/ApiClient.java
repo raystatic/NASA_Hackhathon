5f85bf3c19d79449d7d6314546069f7367a09bd2
raystatic <rahul9650ray@gmail.com>
2019-09-15 12:05:52 +0530

    project setup and API client done

---
+++ app/src/main/java/com/example/nasa/api/ApiClient.java
@@ -0,0 +1,41 @@
+package com.example.nasa.api;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+
+    public static final String BASE_URL = "";
+
+    public static Retrofit retrofit=null;
+
+
+
+    public static Retrofit getApiClient()
+    {
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .readTimeout(120, TimeUnit.SECONDS)
+                .connectTimeout(120, TimeUnit.SECONDS)
+                .build();
+        if (retrofit==null)
+        {
+            retrofit=new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+        }
+
+        return retrofit;
+    }
+
+}
